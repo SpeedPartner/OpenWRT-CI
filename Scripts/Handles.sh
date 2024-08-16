471ed95490e1c3a58b5fd10992553bf9e637789b
SpeedPartner <376705740@qq.com>
2024-08-16 10:54:28 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -77,3 +77,6 @@ fi
 git clone --depth=1 --single-branch https://github.com/linkease/istore.git
 git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
 git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
+
+#Alist
+git clone --depth=1 --single-branch https://github.com/alist-org/alist.git
