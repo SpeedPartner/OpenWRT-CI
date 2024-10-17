c39e24c80d626e5d6d748bea8f303d653d302c7d
SpeedPartner <376705740@qq.com>
2024-10-17 21:35:44 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -26,7 +26,12 @@ git clone https://github.com/sbwml/packages_lang_golang -b 23.x ../feeds/package
 
 #Update rust 1.81.0 or latest version (Fix build for LEDE.)
 rm -rf ../feeds/packages/lang/rust
-git clone https://github.com/immortalwrt/packages/lang/rust ../feeds/packages/lang/rust --depth=1
+git clone --filter=blob:none --sparse https://github.com/immortalwrt/packages ../download/packages
+cd ../download/packages
+git sparse-checkout set lang/rust
+cd ../..
+cp -r download/packages/lang/rust  ../feeds/packages/lang/rust
+cd ./wrt/package/
 
 #UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "$([[ $WRT_REPO == *"lede"* ]] && echo "18.06" || echo "master")"
