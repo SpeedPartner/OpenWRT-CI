7168361349b7815dd45cd3d41b6ddff3d4ed0aa3
SpeedPartner <376705740@qq.com>
2024-10-18 01:14:27 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -31,7 +31,7 @@ cd ../download/packages
 git sparse-checkout set lang/rust
 cd ../..
 cp -r download/packages/lang/rust feeds/packages/lang/rust
-cd ~/wrt/package
+cd package
 
 #UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "$([[ $WRT_REPO == *"lede"* ]] && echo "18.06" || echo "master")"
